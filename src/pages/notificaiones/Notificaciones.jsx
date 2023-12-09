38d365acf9cfd8a17923d6f49b6cd38f6ffc810c
Ramon Ramos <ramon@ual.com>
2023-12-09 18:22:02 +0100

    Added WebSocket basic support

--- src/pages/notificaiones/Notificaciones.jsx
+++ src/pages/notificaiones/Notificaciones.jsx
@@ -1,22 +1,13 @@
 import { ToastContainer, toast } from "react-toastify";
 import { useAuth } from "../../context/AuthContext";
+import WebSocketComponent from "../../websocket/WebSocketComponent";
 
 function Notificaciones() {
   const { username } = useAuth();
   return (
-    <div className="flex flex-col">
-      Notificaiones
-    
-      <button
-        onClick={() => {
-          toast.error("Hola", {
-            position: "bottom-right",
-            autoClose: 3000,
-          });
-        }}
-      >
-        Hola   {username}
-      </button>
+    <div className="flex flex-col content-center">
+      <h1>NOtificaiones</h1>
+      <WebSocketComponent/>
     </div>
   );
 }
